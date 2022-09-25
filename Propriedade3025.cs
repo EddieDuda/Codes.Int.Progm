f6ae692514b61e3948cc0a749f1981b498bcf8db
Eduarda Alexandrino <111926934+EddieDuda@users.noreply.github.com>
2022-09-25 20:57:55 -0300

    Create Propriedade3025.cs

---
+++ Propriedade3025.cs
@@ -0,0 +1,22 @@
+using System;
+class Propriedade3025 {
+  static void Main() {
+    int numero, n1, n2, soma, multiplicacao;
+    
+    Console.WriteLine("Olá, digite um numero entre 1000 e 9999");
+    numero = int.Parse(Console.ReadLine());
+    
+    n1 = numero/100;
+    n2 = numero%100;
+    soma = n1 + n2;
+    multiplicacao = soma * soma;
+  
+    //saida
+    if (multiplicacao == numero) {
+        Console.WriteLine("Esse numero possui a mesma propriedade que 3025");
+    }
+    else{
+        Console.WriteLine("Esse numero não possui a mesma propriedade que  3025");
+    }    
+  }
+}
