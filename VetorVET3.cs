9a3de737bc772dce9bf46fa19c7db4a19ca0fad3
Eduarda Alexandrino <111926934+EddieDuda@users.noreply.github.com>
2022-10-24 16:18:45 -0300

    Create VetorVET3.cs

---
+++ VetorVET3.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace VET3
+{ 
+  public class TestOneArray
+  {  
+    static void Main()
+    {
+       
+
+    int[] VET1 = {1,2,3,4,5,6,7,8,9,10};
+    int[] VET2 = {20,21,22,23,24,25,26,27,28,29}; 
+    int[] VET3 = new int[20];    
+       for (int i = 0, j = 0; i < 10; i++) {
+           VET3[j++] = VET1[i];
+           Console.WriteLine($"Os elementos do vetor VET3 são: { VET3[j-1]}");    
+           VET3[j++] = VET2[i];
+            Console.WriteLine($"Os elementos do vetor VET3 são: { VET3[j-1]}");
+        }
+        
+    }
+  }
+}
