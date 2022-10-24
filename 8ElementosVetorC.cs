c95e17218f931c73cf4be4c3d77fdafd97922870
Eduarda Alexandrino <111926934+EddieDuda@users.noreply.github.com>
2022-10-24 15:41:08 -0300

    Create 8ElementosVetorC.cs

---
+++ 8ElementosVetorC.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace VetorC
+{ 
+  public class TestOneArray
+  {  
+    static void Main()
+    {
+       int i;
+       int[] A = new int[8]; 
+       Console.WriteLine( "Digite 8 números inteiros para A.");
+       for(i = 0; i < 8; i++){
+       A[i] = int.Parse(Console.ReadLine());
+       }
+       
+        int[] B = new int[8]; 
+        Console.WriteLine( "Digite 8 números inteiros para B.");
+        for( i = 0; i < 8; i++){
+        B[i] = int.Parse(Console.ReadLine());
+        } 
+        
+        int[] C = new int[8];
+        for( i = 0; i < 8; i++){
+        C[i] = A[i] + B[i];
+        Console.WriteLine($"Os elementos do vetor C são: {C[i]}");    
+        }
+
+    }
+  }
+}
