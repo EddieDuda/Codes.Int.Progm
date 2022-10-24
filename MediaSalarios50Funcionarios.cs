5f9abc23433299f61e4e971bdd7088e2a841a781
Eduarda Alexandrino <111926934+EddieDuda@users.noreply.github.com>
2022-10-24 15:02:05 -0300

    Create MediaSalarios50Funcionarios.cs

---
+++ MediaSalarios50Funcionarios.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ListaQ3Funcionarios
+{ 
+  public class TestOneArray
+  {  
+    static void Main()
+    {
+       int[] Salarios = new int[50]; 
+       int QuantidadeFuncionarios = 50;
+       int SomaSalarios = 0;
+       double MediaSalarios;
+        
+        Console.WriteLine("Olá, por favor digite o sálario dos funcionarios:");
+       for(int i = 0; i < QuantidadeFuncionarios; i++){
+          Salarios[i] = int.Parse(Console.ReadLine());
+          //Console.Write("{0}\t", Salarios[i]);
+          SomaSalarios = SomaSalarios + Salarios[i];
+       }
+        MediaSalarios = SomaSalarios / QuantidadeFuncionarios;
+        for(int i = 0; i < QuantidadeFuncionarios; i++){
+          if (Salarios[i] > MediaSalarios)
+          Console.Write($"\nEsse sálario {Salarios[i]} é maior do que a média:{MediaSalarios}\n");
+       }
+       
+       
+    }
+  }
+}
