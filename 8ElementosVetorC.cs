b4256347071c21c963e06fb43be5e7e00bc09fc6
Eduarda Alexandrino <111926934+EddieDuda@users.noreply.github.com>
2022-10-30 00:08:15 -0300

    Update 8ElementosVetorC.cs

--- 8ElementosVetorC.cs
+++ 8ElementosVetorC.cs
@@ -1,3 +1,5 @@
+//Lista de Exercícios – Vetores e Matrizes//
+//Questão 2: Fazer um algoritmo que leia dois vetores A e B de 8 elementos cada um e calcula um terceiro vetor C, cujos elementos são resultado da soma dos elementos correspondentes de A e B.//
 using System;
 
 namespace VetorC
